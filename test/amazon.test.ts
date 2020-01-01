caa2ea2abb4ac8178f9a5fbe7d49aa2b48596beb
Jon Stuebe <jstuebe@gmail.com>
2020-01-01 15:11:04 -0700

    adjusted tests

--- test/amazon.test.ts
+++ test/amazon.test.ts
@@ -1,8 +1,19 @@
+import puppeteer, { Browser } from "puppeteer";
 import Scraper from "../src";
+
+let browser: Browser | undefined;
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+});
+afterAll(async () => {
+  if (browser) {
+    await browser.close();
+  }
+});
 
 describe("test various amazon product pages", () => {
   it("adding", async () => {
-    const data = await Scraper("https://www.amazon.com/dp/B07RXXJ8YD");
+    const data = await Scraper("https://www.amazon.com/dp/B07RXXJ8YD", browser);
 
     // amazon product pages change to often to match the same data,
     // so instead we're just matching on if there is data or not
